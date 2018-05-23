7d8a07ced922fe0b7e12cb3ccd3eb5b3354b5f8a
Carlos <carlos@reigndesign.com>
2018-05-22 23:50:25 -0400

    crear operaciones inflow/outflow

--- src/app/admin/operation/operation.component.ts
+++ src/app/admin/operation/operation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { StoreService } from '../../store/store.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { OperationService } from './service/operation.service';
 @Component({
   selector: 'app-operation',
   templateUrl: './operation.component.html',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OperationComponent implements OnInit {
 
-  constructor() { }
+  family;
+  categories;
+  operationForm: FormGroup;
+
+  constructor(private store: StoreService, private fb: FormBuilder, private operationService: OperationService) { }
 
   ngOnInit() {
+    this.family = this.store.getFamily();
+    this.createOperationForm();
+  }
+
+  createOperationForm() {
+    this.operationForm = this.fb.group({
+      group: ['', Validators.required],
+      category: ['', Validators.required],
+      amount: ['', [Validators.required, Validators.minLength(1), Validators.min(1)]],
+      description: [''],
+      occurrenceDate: [''],
+      type : ['']
+    });
+  }
+
+  setGroupSelected(groupId) {
+    this.categories = this.family.categories.filter( category => category.belongsToGroup === groupId);
+  }
+
+  saveOperation() {
+    if (this.operationForm.valid) {
+      const operation = this.operationForm.value;
+      operation.family = this.family._id;
+      this.operationService.create(operation).subscribe(res => {
+        this.operationForm.reset();
+      });
+    }
   }
 
 }
---
+++ src/app/admin/operation/service/operation.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OperationService {
+
+  endpoint = 'operation';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(familyId) {
+    return this.http.get(`${environment.api_base}${this.endpoint}/family/${familyId}`);
+  }
+
+  create(operationData) {
+    return this.http.post(`${environment.api_base}${this.endpoint}`, operationData);
+  }
+
+  delete(categoryId) {
+    return this.http.delete(`${environment.api_base}${this.endpoint}`, { params: { _id: categoryId }});
+  }
+
+  edit(operationData) {
+    return this.http.put(`${environment.api_base}${this.endpoint}`, operationData);
+  }
+
+}
